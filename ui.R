35547c8049aea8806263a95ea8c14ba14facaf22
Harry Williams <hgw1@hotmail.co.uk>
2020-12-08 11:14:01 +0000

    added boxes on overview page

--- ui.R
+++ ui.R
@@ -1,5 +1,24 @@
 dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "Scotland Health"),
+  dashboardSidebar( sidebarMenu(
+    menuItem("Overview", tabName = "overview", icon = icon("dashboard")),
+    menuItem("Life Expectancy", tabName = "life_expectancy", icon = icon("th"))
+  )
+  ),
+  dashboardBody(
+    #First tab
+    tabItem(tabName = "overview",
+            fluidRow(
+              box(title = "Map", width = 8,
+                  "map"),
+              box(title = "Select Variable", width = 4,
+                  "inputs")
+            ),
+            fluidRow(
+              box(title = "Select Year", width = 12,
+                  "year select")
+            )
+      
+    )
+  )
 )
