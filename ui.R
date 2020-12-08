84f71351a0ff24067be65fb97a436be016d67cb4
Harry Williams <hgw1@hotmail.co.uk>
2020-12-08 11:23:35 +0000

    added structure on second tab

--- ui.R
+++ ui.R
@@ -6,19 +6,42 @@ dashboardPage(
   )
   ),
   dashboardBody(
+    tabItems(
     #First tab
     tabItem(tabName = "overview",
             fluidRow(
-              box(title = "Map", width = 8,
-                  "map"),
+              box(title = "Map", 
+                  width = 8,
+                  leafletOutput("map", width = "100%", height = "100%")),
               box(title = "Select Variable", width = 4,
                   "inputs")
             ),
             fluidRow(
-              box(title = "Select Year", width = 12,
+              box(title = "Select Year", 
+                  width = 12,
                   "year select")
             )
       
-    )
+    ),
+    #Second Tab
+    tabItem(tabName = "life_expectancy",
+            fluidRow(
+              box(title = "Select Variables", 
+                  width = 12
+                  )
+              
+            ),
+            fluidRow(
+              box(title = "Difference From Mean",
+                  width = 6),
+              box(title = "Life Expectancy Over Time",
+                  width = 6)
+              ),
+            fluidRow(box(title = "Top 5 Councils",
+                         width = 6),
+                     box(title = "Bottom 5 Councils",
+                         width = 6))
+            )
+  )
   )
 )
